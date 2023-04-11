67fc3405eb7fcbc47e6b5e548e54ed4a300e94e1
gaultierlecaillon <gaultier.lecail@gmail.com>
2023-04-11 11:22:30 +0000

    doing action until grab

--- src/ia_package/ia_package/ia_node.py
+++ src/ia_package/ia_package/ia_node.py
@@ -6,8 +6,12 @@ from std_msgs.msg import Bool
 
 
 class IANode(Node):
+    action_name = None
+    action_param = None
+
     def __init__(self):
         super().__init__('ia_node')
+
         self.actions_dict = []
         self.parse_strat()
 
@@ -18,36 +22,53 @@ class IANode(Node):
     def master_callback(self):
         self.doCurrentAction()
 
-    def callback_waiting_tirette(self, msg):
-        if msg.data:
-            self.get_logger().info(f"msg from node {msg.data}")
+    def callback_waiting_tirette(self, msg, param):
+        if msg.data == bool(param):
             self.update_current_action_status('done')
+            self.destroy_subscription(self.subscriber_)  # Unsubscribe from the topic
 
     def doCurrentAction(self, ):
         current_action = self.actions_dict[0]
         action = current_action['action']
+        self.action_name, self.action_param = list(action.items())[0]
+
         self.get_logger().info(f"[Current Action] {current_action['action']} ({current_action['status']})")
 
         if current_action['status'] == "pending":
-            action_name, action_param = list(action.items())[0]
-            getattr(self, action_name)(action_param)  # Call the method using action_name
+            getattr(self, self.action_name)(self.action_param)  # Call the method using self.action_name
             self.update_current_action_status('ongoing')
 
-    def waiting_tirette(self, param):
-        print(f"Inside waiting_tirette function with parameter: {param}")
+    # ACTIONS METHODS
+    ''' 
+    name: waiting_tirette
+    desc: Waiting ther user to insert the tirette     
+    '''
 
-        # Tirette Subscriber
-        self.subscriber_ = self.create_subscription(Bool, "tirette_topic", self.callback_waiting_tirette, 1)
+    def waiting_tirette(self, param):
+        self.subscriber_ = self.create_subscription(
+            Bool,
+            "tirette_topic",
+            lambda msg: self.callback_waiting_tirette(msg, param), 1)
+
+    ''' 
+    name: grab
+    param: 
+    desc: got to (x,y,r) and start to grab the stuff
+    '''
+
+    def grab(self, param):
+        print("Doing action grab with param:", param)
 
     def update_current_action_status(self, status):
         if status == "done":
             self.actions_dict.pop(0)
+            self.get_logger().info(f"[Action Done!] {self.action_name}")
         else:
             self.actions_dict[0]['status'] = status
 
     def parse_strat(self):
-        #with open('../resource/strat.json') as file:
-        with open('/home/edog/ros2_ws/install/ia_package/share/ia_package/strat.json') as file:
+        # with open('../resource/strat.json') as file:
+        with open('/home/edog/ros2_ws/src/ia_package/resource/strat.json') as file:
             config = json.load(file)
 
         self.get_logger().info(f"[Loading Strategy] {config['name']} ({config['description']})")
@@ -61,6 +82,7 @@ class IANode(Node):
                         'status': 'pending',
                     }
                 )
+
 
 def main(args=None):
     rclpy.init(args=args)
