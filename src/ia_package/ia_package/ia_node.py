e9d2e20eb7d52ba3a1d7c4debf0fa23d09627176
gaultierlecaillon <gaultier.lecail@gmail.com>
2023-04-11 16:28:29 +0000

    ia node working

--- src/ia_package/ia_package/ia_node.py
+++ src/ia_package/ia_package/ia_node.py
@@ -11,41 +11,40 @@ from robot_interfaces.srv import CmdPositionService
 class IANode(Node):
     action_name = None
     action_param = None
+    current_action_already_printed = False
 
     def __init__(self):
         super().__init__('ia_node')
-
         self.actions_dict = []
-        self.parse_strat()
+        self.load_strategy_from_file()
 
-        # Node
         self.number_timer_ = self.create_timer(0.1, self.master_callback)
-        self.get_logger().info("IA Node is awake ! (⌐■_■) 𝘴𝘶𝘱 𝘣𝘳𝘢 ?")
+        self.get_logger().info("\033[38;5;208mIA Node is running!\n\n\t\t\t (⌐■_■) 𝘴𝘶𝘱 𝘣𝘳𝘢 ?\033[0m\n")
 
     def master_callback(self):
-        self.doCurrentAction()
+        self.execute_current_action()
 
     def callback_waiting_tirette(self, msg, param):
         if msg.data == bool(param):
             self.update_current_action_status('done')
             self.destroy_subscription(self.subscriber_)  # Unsubscribe from the topic
 
-    def doCurrentAction(self, ):
-        current_action = self.actions_dict[0]
-        action = current_action['action']
-        self.action_name, self.action_param = list(action.items())[0]
+    def execute_current_action(self):
+        if len(self.actions_dict) > 0:
+            current_action = self.actions_dict[0]
+            action = current_action['action']
+            self.action_name, self.action_param = list(action.items())[0]
 
-        self.get_logger().info(f"[Current Action] {current_action['action']} ({current_action['status']})")
+            if not self.current_action_already_printed:
+                self.get_logger().info(f"[Current Action] {self.action_name} ({current_action['status']})")
+                self.current_action_already_printed = True
 
-        if current_action['status'] == "pending":
-            getattr(self, self.action_name)(self.action_param)  # Call the method using self.action_name
-            self.update_current_action_status('ongoing')
-
-    # ACTIONS METHODS
-    ''' 
-    name: waiting_tirette
-    desc: Waiting ther user to insert the tirette     
-    '''
+            if current_action['status'] == "pending":
+                getattr(self, self.action_name)(self.action_param)
+                self.update_current_action_status('ongoing')
+        else:
+            self.get_logger().info(f"[Match done] No more actions to exec)")
+            rclpy.shutdown()
 
     def waiting_tirette(self, param):
         self.subscriber_ = self.create_subscription(
@@ -53,36 +52,16 @@ class IANode(Node):
             "tirette_topic",
             lambda msg: self.callback_waiting_tirette(msg, param), 1)
 
-    ''' 
-    name: grab
-    param: 
-    desc: got to (x,y,r) and start to grab the stuff
-    '''
-
     def grab(self, param):
-        print("TODO Doing action grab with param:", param)
-
-    ''' 
-    name: goto
-    param: 
-    desc: got to (x,y,r)
-    '''
+        self.get_logger().info(f"TODO: Performing grab action with param: {param}")
 
     def goto(self, param):
-        print("Doing action goto with param:", param)
-        '''
-        coordinate = [int(num) for num in param.split(',')]
-        self.goto_publisher_ = self.create_publisher(CmdPositionService, "cmd_position_service", 10)
+        self.get_logger().info(f"[Exec Action] goto with param: {param}")
 
-        position = Position(
-            x=coordinate[0],
-            y=coordinate[1],
-            r=coordinate[2])
-        self.goto_publisher_.publish(position)
-        '''
         client = self.create_client(CmdPositionService, "cmd_position_service")
         while not client.wait_for_service(0.25):
-            self.get_logger().warn("Waiting for Server to finsh goto...")
+            self.get_logger().warn("Waiting for Server to finish goto...")
+
         request = CmdPositionService.Request()
         request.x = 0
         request.y = 0
@@ -92,28 +71,30 @@ class IANode(Node):
         future.add_done_callback(
             partial(self.callback_goto))
 
-        print("Publish:", request, "to cmd_position_service")
+        self.get_logger().info(f"[Publish] {request} to cmd_position_service")
 
     def callback_goto(self, future):
         try:
             response = future.result()
             if response.success:
-                self.get_logger().info(f"Action Done ! {response}")
+                self.get_logger().info(f"[Current Action] Done ! {response}")
                 self.update_current_action_status('done')
             else:
                 self.get_logger().info(f"Something went wrong with response: {response}")
 
         except Exception as e:
+
             self.get_logger().error("Service call failed %r" % (e,))
 
     def update_current_action_status(self, status):
         if status == "done":
             self.actions_dict.pop(0)
-            self.get_logger().info(f"[Action Done!] {self.action_name}")
         else:
             self.actions_dict[0]['status'] = status
+        self.current_action_already_printed = False
 
-    def parse_strat(self):
+
+    def load_strategy_from_file(self):
         with open('/home/edog/ros2_ws/src/ia_package/resource/strat.json') as file:
             config = json.load(file)
 
@@ -129,14 +110,12 @@ class IANode(Node):
                     }
                 )
 
-
 def main(args=None):
     rclpy.init(args=args)
     ia_node = IANode()
     rclpy.spin(ia_node)
     ia_node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
