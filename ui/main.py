83f2d9b8c4ee74f75cb4fdc5212ba2d5838eadd3
gaultierlecaillon <gaultier.lecail@gmail.com>
2023-05-14 23:12:00 +0000

    new button

--- ui/main.py
+++ ui/main.py
@@ -8,12 +8,8 @@ root = tk.Tk()
 root.geometry('300x200')
 
 button_commands = [
-    ('Start', 'ros2 launch launcher robot.launch.py strat:=strat'),
-    ('Print working directory', 'pwd'),
-    ('Echo Hello', 'echo Hello'),
-    ('Get date', 'date'),
-    ('Who am I', 'whoami'),
-    ('System info', 'uname -a')
+    ('start.json', 'ros2 launch launcher robot.launch.py strat:=strat'),
+    ('10forward.json', 'ros2 launch launcher robot.launch.py strat:=10forward'),
 ]
 
 for i, (button_text, command) in enumerate(button_commands):
